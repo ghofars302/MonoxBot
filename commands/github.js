c53c19dcceecfefc81a2b36806649eef8dd4fb77
Monodoxy <7972820+ghofars302@users.noreply.github.com>
2018-03-06 19:45:07 +0700

    oWo -- Update missing things

---
+++ commands/github.js
@@ -0,0 +1,17 @@
+exports.run = async (client, message, args, level) => {
+  message.channel.send('Bot repo : https://github.com/ghofars302/MonoxBot')
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['git'],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "github",
+  category: "unity",
+  description: "View this bot repo, if You want look sources and selfhost this bot",
+  usage: "github || git"
+};
--- commands/help.js
+++ commands/help.js
@@ -2,12 +2,26 @@ exports.run = (client, message, args, level) => {
   if (!args[0]) {
     message.reply('https://monoxbot.glitch.me/');
   } else {
-    // Show individual command's help.
     let command = args[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
       if (level < client.levelCache[command.conf.permLevel]) return;
-      message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\naliases:: ${command.conf.aliases.join(", ")}\n= ${command.help.name} =`, {code:"asciidoc"});
+      message.channel.send({embed: {
+        title: 'Information about command:  ' + command.help.name,
+        fields: [{
+          name: 'Description',
+          value: command.help.description
+        },
+        {
+          name: 'Usage',
+          value: command.help.usage
+        },
+        {
+          name: 'Alias',
+          value: command.conf.aliases.join(", ")
+        }
+        ],
+      }});
     }
   }
 };
@@ -15,13 +29,13 @@ exports.run = (client, message, args, level) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["h", "halp"],
+  aliases: ['h', 'halp'],
   permLevel: "User"
 };
 
 exports.help = {
   name: "help",
   category: "System",
-  description: "Displays all the available commands for your permission level.",
+  description: "Look command list at Bot website or look Invidual command.",
   usage: "help [command]"
 };
