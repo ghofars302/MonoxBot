6de1ebe50421f1e14b7dc1d2c074caa19f27c25d
Monodoxy <7972820+ghofars302@users.noreply.github.com>
2018-03-06 12:38:55 +0700

    Update urban command
    add Text limit and add vote up and down

--- commands/urban.js
+++ commands/urban.js
@@ -18,31 +18,35 @@ exports.run = async (client, message, args, level) => {
             }
           }});
         } else {
+          var out = entries[0].definition;
+          out.length > 1024 ? out = out.substring(0, 1024) : out;
+          var ex = entries[0].example;
+          ex.length > 1024 ? ex = ex.substring(0, 1024) : out;
           if(!entries[0].example) return msg.edit({embed: {
             title: ":book: " + entries[0].word,
             fields: [{
               name: 'Results',
-              value: entries[0].definition,
+              value: out
             }],
             footer: {
               icon_url: 'http://www.packal.org/sites/default/files/public/styles/icon_large/public/workflow-files/florianurban/icon/icon.png?itok=sMaOFyEA',
-              text: 'Urban Dictionary'
+              text: '👍 ' + entries[0].thumbs_up + ' 👎 ' + entries[0].thumbs_down +' Urban Dictionary'
             }
           }});
           msg.edit({embed: {
             title: ":book: " + entries[0].word,
             fields: [{
               name: 'Results',
-              value: entries[0].definition,
+              value: out
             },
             {
               name: 'Example',
-              value: entries[0].example
+              value: ex
             }
             ],
             footer: {
               icon_url: 'http://www.packal.org/sites/default/files/public/styles/icon_large/public/workflow-files/florianurban/icon/icon.png?itok=sMaOFyEA',
-              text: 'Urban Dictionary'
+              text: '👍 ' + entries[0].thumbs_up + ' 👎 ' + entries[0].thumbs_down + ' Urban Dictionary'
             }
           }})
         };
