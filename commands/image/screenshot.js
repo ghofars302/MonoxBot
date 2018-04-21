7e9e2eb4296f9f142dfeb98fffb536bc1d4c3513
Monodoxy <ghofarsuprapto@gmail.com>
2018-04-21 14:10:48 +0700

    u w0t

--- commands/image/screenshot.js
+++ commands/image/screenshot.js
@@ -1,9 +1,6 @@
-const commando = require('discord.js-commando');
-const api = require('puppeteer');
-const jimp = require('jimp');
+const MonoxHandler = require('../../const/MonoxHandler.js');
 
-
-class ScreenshotCommand extends commando.Command {
+class ScreenshotCommand extends MonoxHandler {
 	constructor(client) {
 		super(client, {
 			name: 'screenshot',
@@ -19,32 +16,37 @@ class ScreenshotCommand extends commando.Command {
 		});
 	}
 
-  async run(msg, argString) {
-    if(!argString) return msg.channel.send('```m!ss (website link)\n\ntake a screenshot of website```')
-		async function screenshot(value) {
+	async run(msg, argString) {
+		if (!argString) return msg.channel.send('\`\`\`m!screenshot (url link || website url)\n\nTake screenshot from website.\`\`\`');
+		msg.channel.startTyping();
+		if (!this.utils.isHttp(argString)) {
+			this.TakeScreenshot(this.client, msg, 'http://' + argString);
+		} else {
+			this.TakeScreenshot(this.client, msg, argString);
+		};
+		msg.channel.stopTyping(true);
+	}
+	
+	async TakeScreenshot(client, msg, value) {
 			try {
-				const browser = await api.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+				const browser = await this.puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 				const page = await browser.newPage();
 				await page.setViewport({width: 1920, height: 1080});
 				await page.goto(value, {"waitUntil" : "networkidle0"});
 				let result = await page.screenshot();
-				jimp.read(result, function (error, images) {
-					images.getBuffer(jimp.MIME_PNG, function (error, buffer) {
-						msg.channel.send(({files: [{name: "screenshot.png", attachment: buffer}]}));
-					});
+				this.gm(result)
+					.toBuffer('PNG', (err, buffer) => {
+						if (err) return msg.channel.send(':warning: ``Unable to upload screenshot\`\` \`\`\`' + err + '\`\`\`');
+						msg.channel.send({files: [{name: 'screenshot.png', attachment: buffer}]})
 				});
-				msg.channel.stopTyping(true);
 				await browser.close();
 			} catch (error) {
-				msg.channel.send(':warning: ``' + error + '``');
-				console.error(error);
-				msg.channel.stopTyping(true);
+				msg.channel.send(':warning: \`\`Unable to take screenshot.\`\` \`\`\`' + error + '\`\`\`');
+				console.log(error);
 			}
 		}
-		msg.channel.startTyping();
-		if(!this.client.utils.isURL(argString)) return screenshot("https://" + argString);
-		screenshot(argString);
-  }
+	}
 };
 
 module.exports = ScreenshotCommand;
+
