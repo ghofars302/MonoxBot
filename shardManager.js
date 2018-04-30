3ec2787896ca5f67b74fd60d7319f3caa3389708
Monodoxy <ghofarsuprapto@gmail.com>
2018-05-01 02:24:50 +0700

    no need

--- shardManager.js
+++
@@ -1,9 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager(`${__dirname}/ShardScript.js`, { totalShards: 1, token: process.env.TOKEN });
-
-manager.spawn();
-manager.on('launch', shard => console.log(`Successfully launched shard ${shard.id}`));
-
-process.on('unhandledRejection', (err) => {
-  console.log('[Shard Manager] ' + (err && err.stack) || err);
-});
