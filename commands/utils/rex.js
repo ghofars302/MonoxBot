c56dc792ebf48cad76ab7fdf326eb9783e256a82
Monodoxy <ghofarsuprapto@gmail.com>
2018-07-06 18:17:46 +0700

    Beautify

--- commands/utils/rex.js
+++ commands/utils/rex.js
@@ -1,18 +1,20 @@
-const {stripIndent} = require('common-tags');
+const {
+    stripIndent
+} = require('common-tags');
 const API = require('../../modules/rex.json');
 
 module.exports = {
-	description: 'Evaluates code on rextester.com',
-	category: 'Utils',
+    description: 'Evaluates code on rextester.com',
+    category: 'Utils',
     args: '<Language> <Code>',
     ownerOnly: true,
-	run: async function (ctx, args) {
+    run: async function (ctx, args) {
         if (args.length < 1) return `\`\`\`${ctx.bot.config.prefix}rex <Language> <code>\n\nSubcommands:\n-list (Get rextester language supported-list)\n\nRun or test your code in rextester.com\`\`\``;
 
         const Lang = args.shift().toLowerCase();
         const code = args.join(' ');
 
-        if (Lang.toLowerCase() === 'list') return stripIndent`
+        if (Lang.toLowerCase() === 'list') return stripIndent `
             \`\`\`
             Rextester language list:
             - C#            - Lisp          - Ocaml
@@ -48,10 +50,10 @@ module.exports = {
                 },
                 json: true
             });
-    
+
             return `\`\`\`js\n${res['Result'] ? ctx.bot.api.Util.escapeMarkdown(res['Result'], true, true) : res['Errors'] ? ctx.bot.api.Util.escapeMarkdown(res['Errors'], true, true) : 'Empty response'}\`\`\``;
         } catch (error) {
             return `:x: \`There a Error while calling the API\``;
-        }    
-	}
+        }
+    }
 };
