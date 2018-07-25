93b2be038918ffdb5c4dd3f724548aeda3b80348
Monodoxy <ghofarsuprapto@gmail.com>
2018-07-25 22:13:56 +0700

    Update duckduckgo.js

--- commands/Utils/duckduckgo.js
+++ commands/Utils/duckduckgo.js
@@ -11,7 +11,7 @@ module.exports = {
             headers: {
                 'Authorization': process.env.matmen,
                 'Content-Type': 'application/json',
-                'User-Agent': `MonoxFramework ${require('../package.json')['version']} (MonoxBot)`
+                'User-Agent': `MonoxFramework ${require('../../package.json')['version']} (MonoxBot)`
             }
         }
         
