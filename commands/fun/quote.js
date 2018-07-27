e2c36289b464e710c5c5a3a52e061d22d116e8f7
Monodoxy <ghofarsuprapto@gmail.com>
2018-07-27 17:01:04 +0700

    bored af

--- commands/fun/quote.js
+++ commands/fun/quote.js
@@ -26,7 +26,7 @@ module.exports = {
             try {
                 const messageObject = await ctx.channel.messages.fetch(args[1]);
                 user = messageObject.author;
-                message.content = messageObject.content !=== '' ? messageObject.content.replace(regex, (match, id) => (ctx.guild && ctx.guild.members.has(id) && ctx.guild.members.get(id).nickname) ? `[@${ctx.guild.members.get(id).nickname}]`: ctx.users.has(id) ? `[@${ctx.users.get(id).username}]` : match) : '';
+                message.content = messageObject.content !== '' ? messageObject.content.replace(regex, (match, id) => (ctx.guild && ctx.guild.members.has(id) && ctx.guild.members.get(id).nickname) ? `[@${ctx.guild.members.get(id).nickname}]`: ctx.users.has(id) ? `[@${ctx.users.get(id).username}]` : match) : '';
                 timestamp = message.createdTimestamp;
             
                 if (messageObject.embeds) {
