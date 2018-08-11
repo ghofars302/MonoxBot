cf17f7ec40cfea7def4cb80a6bb0f4226d0e6fd1
Monox <ghofarsuprapto@gmail.com>
2018-08-11 07:55:01 +0700

    Update rpc.js

--- commands/adminOnly/rpc.js
+++ commands/adminOnly/rpc.js
@@ -7,41 +7,42 @@ module.exports = {
 		if (args.length < 1) return `\`\`\`${ctx.prefix}rpc <Javascript code> | [pull | reload | restart]\n\nSubcommands:\n-pull (Pull bot update from Bot's repo)\n-reload (Reload all commands)\n-restart (Restart bot)\`\`\``;
 
 		switch (args[0].toLowerCase()) {
-			case 'pull':
-				try {
+			case 'pull': 
+				let result;
+				
+				try { 
 					const { exec } = require('child_process');
 
-					exec('git pull origin master', err => {
-						if (err) throw err;
-					})
-
-					ctx.reply(':white_check_mark: `Git pulled!`')
+					await exec('git pull origin master', {}, (err, res) => {
+						if (err) throw err; 
+						result = res;
+					}) 
+					
+					
+					return `:white_check_mark: \`Operation success\`\n\`\`\`js\n${await result.stdout}\`\`\``;
 				} catch (error) {
-					ctx.reply(':x: `There a error while pull the git`');
+					return `:x: \`There a error while pull the git\`\n\`\`\`js\n${await error.stderr}\`\`\``;
 				}
-				break;
 			case 'reload':
 				try {
 					await ctx.main.shard.broadcastEval(`
 						this.bot.commands = this.bot.ResourceLoader.loadCommands();
 
-						true
+						true;
 					`);
-
-					ctx.reply(':white_check_mark: `Command reloaded on all shards`')
-				} catch (error) {
-					ctx.reply(':x: `There a error while broadcasting to other shards`');
+					
+					return ':white_check_mark: `Command reloaded on all shards`';
+				} catch (error) { 
+					return ':x: `There a error while broadcasting to other shards`';
 				}
-				break;
 			case 'restart':
 				try {
 					await ctx.reply('Restarting bot..');
 
-					await ctx.main.shard.broadcastEval(`process.exit()`);
+					return ctx.main.shard.broadcastEval(`process.exit()`);
 				} catch (error) {
-					ctx.reply(':x: `There a error while broadcasting to other shards`');
+					return ':x: `There a error while broadcasting to other shards`'
 				}
-				break;
 			default:
 				try {
 					const argsString = args.join(' ')
@@ -49,11 +50,10 @@ module.exports = {
 
 					const result = await ctx.main.shard.broadcastEval(`${code}`);
 
-					await ctx.reply(`\`\`\`${JSON.stringify(result, null, 4) || '[]'}\`\`\``);
+					return `\`\`\`${JSON.stringify(result, null, 4) || '[]'}\`\`\``
 				} catch (error) {
-					ctx.reply(`:x: \`There a error while broadcasting to other shards\` \`\`\`${error}\`\`\``);
+					return `:x: \`There a error while broadcasting to other shards\` \`\`\`${error}\`\`\``
 				}
-				break;
 		}
 	}
 }
